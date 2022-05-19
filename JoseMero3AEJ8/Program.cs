9459fa1b371d8c144aef46aaa9770631e4239569
josemeroq <93845854+josemeroq@users.noreply.github.com>
2022-05-18 20:55:13 -0500

    Add files via upload

---
+++ JoseMero3AEJ8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JoseMero3AEJ8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            multiplo mu = new multiplo();
+            mu.procesomultiplo();
+        }
+    }
+}
---
+++ JoseMero3AEJ8/multiplo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoseMero3AEJ8
+{
+    public class multiplo
+    {
+        private int m { get; set; }
+        public multiplo()
+        {
+            this.m = 1;
+            
+        }
+        public void procesomultiplo()
+        {
+            m = m - 1;
+            do
+            {
+                m = m + 3;
+                Console.WriteLine(m);
+            } while (m != 3000);
+        }
+        
+    }
+}
